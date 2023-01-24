135da8e303ba1e5b33ab5abceae6a781acbc0864
Adrien Hors <adrienhors1@gmail.com>
2023-01-24 17:34:46 +0100

    First Pages  / views

---
+++ merchex/listings/views.py
@@ -0,0 +1,16 @@
+
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def hello(request):
+    return HttpResponse('<h1>Hello Django!</h1>')
+
+def about(request):
+    return HttpResponse('<h1>À propos</h1> <p>Nous adorons la merch !</p>')
+
+def listings(request):
+    return HttpResponse('<h1>Listings</h1> <p>Tous les produits !</p>')
+
+def contact(request):
+    return HttpResponse("<h1>Contact</h1> <p>N'hésitez pas à nous contacter</p>")
