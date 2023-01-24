cc783f0b3cf0fb2e999d934bb848dbc4e357cc09
Adrien Hors <adrienhors1@gmail.com>
2023-01-24 18:39:34 +0100

    Migrations / First Model

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -2,9 +2,28 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from listings.models import Band
+from listings.models import Annonce
+
 
 def hello(request):
-    return HttpResponse('<h1>Hello Django!</h1>')
+    bands = Band.objects.all()
+    annonces = Annonce.objects.all()
+    return HttpResponse(f"""
+        <h1>Hello Django !</h1>
+        <p>Mes groupes préférés sont:<p>
+        <ul>
+            <li>{bands[0].name}</li>
+            <li>{bands[1].name}</li>
+            <li>{bands[2].name}</li>
+        </ul>
+        <p>Dernières annonces:<p>
+        <ul>
+            <li>{annonces[0].title}</li>
+            <li>{annonces[1].title}</li>
+            <li>{annonces[2].title}</li>
+        </ul>
+""")
 
 def about(request):
     return HttpResponse('<h1>À propos</h1> <p>Nous adorons la merch !</p>')
